4a92974f111b2527c6cbc383671664766f6b59f7
mkozlowskiAzimuthe <michal.kozlowski@azimuthe.com>
2023-01-20 13:42:58 +0100

    docs

--- emargo_crawl.py
+++ emargo_crawl.py
@@ -9,14 +9,26 @@ from typing import List
 MAIN_URL = "http://emargo.pl"
 
 def process_data(item_data: str) -> dict:
+    """Proces item data, remove unused characters for loading into dict, flatten dict one dimension 
+
+    Args:
+        item_data (str): Unprocessed text from javascript containing data about item, npcs, quests, drops
+
+    Returns:
+        dict: Processed item dict
+    """
+    
     # proces data for loading
     replace_keys = ['item', 'monster', 'npc', 'quest']
     item_data = item_data.replace(";var R =", '').replace("\n", '')
     for key in replace_keys:
         item_data = re.sub(f"\s{key}:\s", f'"{key}":', item_data)
-    loaded = json.loads(item_data[:-1])
+
+    # slice last character for proper loading, remove ';'
+    loaded: dict = json.loads(item_data[:-1])
+
     # flaten structure
-    for element in list(loaded.keys()):
+    for element in loaded.keys():
         temp_inner_data = []
         for inner_key in loaded[element]:
             temp_inner_data.append(loaded[element][inner_key])
@@ -25,10 +37,29 @@ def process_data(item_data: str) -> dict:
 
 
 def process_classes(classes_string: str) -> List[str]:
+    """Change initial of classes string to list of classes
+
+    Args:
+        classes_string (str): String containing letters of classes
+
+    Returns:
+        List[str]: List of classes coresponding to initials
+    """
+
     classes_dict = {"p": "Paladyn", "w": "Wojownik", "b": "Tancerz Ostrzy", "m": "Mag", "t": "Tropiciel", "h": "Łowca"}
     return [classes_dict[initial] for initial in classes_string]
 
 def stat_transform(item_loaded_data: dict) -> dict:
+    """Transform stat info into dictionary, map rarity and binding to useable form, flatten items if possible
+
+    Args:
+        item_loaded_data (dict): item dict with unchanged data
+
+    Returns:
+        dict: Item with procesed stats, value
+    """
+
+
     TYPES = ['heroic', 'unique', 'legendary']
     BINDING = ['binds', 'soulbound']
     # change stats
@@ -55,13 +86,22 @@ def stat_transform(item_loaded_data: dict) -> dict:
                     stats_dict['binding'] = element_split[0]
         item['stats'] = stats_dict
 
-    # flaten items
+    # flaten items, list not needed
     if len(item_loaded_data['item']) == 1:
         item_loaded_data['item'] = item_loaded_data['item'][0]
 
     return item_loaded_data
 
-def get_item_data(item_path) -> dict:
+def get_item_data(item_path: str) -> dict:
+    """Retrieve data from item page and pass to inner function, sleep if request status is not correct and retry
+
+    Args:
+        item_path (str): href to item page
+
+    Returns:
+        dict: procesed dictionary with item data
+    """
+
     item_page_request = requests.get(item_path)
     if item_page_request.status_code != 200:
         time.sleep(20)
@@ -70,10 +110,17 @@ def get_item_data(item_path) -> dict:
     item_data = item_soup.find("script", string=re.compile(";var R.*")).text
     return stat_transform(process_data(item_data))
 
-def main():
+def main() -> None:
+    """Main function for retriving data from `http://emargo.pl`, iterates through item types and items of given type.
+    Saves results to `emargo.json` file
+
+    """
     items = []
+    # start from page of item types
     item_page = BeautifulSoup(requests.get(f"{MAIN_URL}/przedmioty/").text, features="lxml")
     pbar = tqdm(item_page.find_all("a", {"href": re.compile("\/przedmioty\/dla.*")}))
+    
+    # iterate through links of items types
     for element_index, element_a in enumerate(pbar):
         items_list_request = requests.get(MAIN_URL + element_a['href'])
         if items_list_request.status_code != 200:
@@ -81,15 +128,19 @@ def main():
             items_list_request = requests.get(MAIN_URL + element_a['href'])
         soup_items = BeautifulSoup(items_list_request.text, features="lxml")
         items_tags = soup_items.find_all("a", {"href": re.compile("\/przedmiot\/.*")})
+        
+        # get data from items of given type
         for item_index, item in enumerate(items_tags):
             pbar.set_description(f"Profession: {element_a['href'].split('/')[-2]}, Item Type: {element_a.text}, Item count: {item_index+1}/{len(items_tags)}")
             time.sleep(0.5)
             data_dict = get_item_data(MAIN_URL + item['href'])
             data_dict['item']['type'] = element_a.text
             items.append(data_dict)
+    
+    # save to json file
     with open("emargo.json", "w", encoding='utf-8') as f:
         json.dump(items, f, ensure_ascii=False)
-    return items
+    return
 
 if __name__ == "__main__":
     main()
